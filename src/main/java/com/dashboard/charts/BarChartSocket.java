39398686c70e9e7dc9d561592982cfdda1bdbbf3
ThirupathiReddy V <vajjala.thirupathireddy@gmail.com>
2016-10-26 22:36:58 +0530

    dynamic

--- src/main/java/com/dashboard/charts/BarChartSocket.java
+++ src/main/java/com/dashboard/charts/BarChartSocket.java
@@ -1,7 +1,8 @@
-package com.pretech.test.websockets;
+package com.dashboard.charts;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Random;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
@@ -10,9 +11,9 @@ import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint("/websocket")
-public class WebSocketTest {
+public class BarChartSocket {
 
-    public static int[][] data = { { 10, 20, 30, 40, 50 }, { 15, 25, 35, 45, 55 },{ 44,33,22,11,22 }, { 99,88,77,66,44 } , { 32,99,22,44,22 } };
+    public static int[][] data = { { 10, 20, 30, 40 }, { 15, 25, 35, 45 },{ 44,33,22,11 }, { 99,88,77,66 } , { 32,99,44,22 } };
 
     @OnMessage
     public void onMessage(String message, Session session) throws IOException, InterruptedException {
@@ -26,6 +27,16 @@ public class WebSocketTest {
         }
         System.out.println("Sending data array "+Arrays.toString(data[i]));
         session.getBasicRemote().sendText(Arrays.toString(data[i]));
+       
+        while (true) {
+            i = new Random().nextInt(4);
+            System.out.println("Sending data array " + Arrays.toString(data[i]));
+            session.getBasicRemote().sendText(Arrays.toString((data[i])));
+            
+            Thread.sleep(5000);
+            
+        }
+        
 
     }
 
--- src/main/java/com/dashboard/charts/PieChartWebSocket.java
+++ src/main/java/com/dashboard/charts/PieChartWebSocket.java
@@ -1,7 +1,8 @@
-package com.pretech.test.websockets;
+package com.dashboard.charts;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Random;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
@@ -12,9 +13,9 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint("/piechart")
 public class PieChartWebSocket {
 
-    public static String[][][] data = { { { "Windows", "40" }, { "MacOS", "20" }, { "Linux", "30" }, { "Others", "10" } },
-        { { "Windows", "50" }, { "MacOS", "20" }, { "Linux", "30" }, { "Others", "0" } },
-            { { "Windows", "20" }, { "MacOS", "20" }, { "Linux", "30" }, { "Others", "30" } } };
+    public static String[][][] data = { { { "SML", "40" }, { "TML", "20" }, { "FML", "30" }, { "Others", "10" } },
+            { { "SML", "50" }, { "TML", "20" }, { "FML", "30" }, { "Others", "0" } },
+            { { "SML", "20" }, { "TML", "20" }, { "FML", "30" }, { "Others", "30" } } };
 
     @OnMessage
     public void onMessage(String message, Session session) throws IOException, InterruptedException {
@@ -26,8 +27,14 @@ public class PieChartWebSocket {
         } catch (NumberFormatException ne) {
             System.out.println("invalid input received");
         }
-        System.out.println("Sending data array " + Arrays.deepToString(data[i]));
-        session.getBasicRemote().sendText(Arrays.deepToString((data[i])));
+        while (true) {
+            i = new Random().nextInt(3);
+            System.out.println("Sending data array " + Arrays.deepToString(data[i]));
+            session.getBasicRemote().sendText(Arrays.deepToString((data[i])));
+            
+            Thread.sleep(5000);
+            
+        }
 
     }
 
